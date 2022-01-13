f7c643275b89a08ea203e010b9a3a7df74249d73
Nicole Steadman <nsteadman@gmail.com>
2022-01-13 11:21:38 -0800

    commit tree d83038670119c39da987b47b130bb2ff392d522c

--- src/components/ColdStorage.js
+++ src/components/ColdStorage.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import HostList from "./HostList";
 
-function ColdStorage() {
+function ColdStorage({ hosts, onHostClick }) {
   return (
     <Segment.Group className="HQComps">
       <Segment compact>
         <h3 className="labels">ColdStorage</h3>
       </Segment>
       <Segment compact>
-        {/* Cold Storage contains hosts....but how? Directly? Or is there something else we could use to contain them... */}
+        <HostList hosts={hosts} onHostClick={onHostClick} />
       </Segment>
     </Segment.Group>
   );
 }
+
 
 export default ColdStorage;
--- src/components/HostList.js
+++ src/components/HostList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import Host from "./Host";
 
-function HostList() {
-  return (
-    <Card.Group itemsPerRow={6}>{/* What do you think, partner? */}</Card.Group>
-  );
+function HostList({ hosts, onHostClick }) {
+  const hostList = hosts.map((host) => (
+    <Host key={host.id} host={host} onHostClick={onHostClick} />
+  ));
+
+  return <Card.Group itemsPerRow={6}>{hostList}</Card.Group>;
 }
+
 
 export default HostList;
